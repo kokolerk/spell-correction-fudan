d42a3a0ce09986d6d0f3ac2696a9615846a81c01
Jiaqi Wang <68948604+kokolerk@users.noreply.github.com>
2022-05-06 18:41:35 +0800

    Delete sid_homework-1 directory

--- sid_homework-1/program/run_me.py
+++
@@ -1,8 +0,0 @@
-from spell_correct import spell_correction
-from eval import evaluation
-
-if __name__ == "__main__":
-    # best config
-    s=spell_correction(LM='2', corpus=True,channel=False,editdistance=2)
-    s.word_correct()
-    evaluation()
